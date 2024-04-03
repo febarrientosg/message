11ac359bf148235b7a0a81827f1c2aa587f9d854
febarrientosg <22461164+febarrientosg@users.noreply.github.com>
2024-04-03 18:56:48 +0900

    new

--- index.js
+++ index.js
@@ -12,19 +12,17 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
   pixelDensity(2);
   textSize(17);
+  let sketchContainer = document.getElementById("sketch-container");
+  let canvas = createCanvas(sketchContainer.offsetWidth, windowHeight - 36); // Set height as per your requirement
+  canvas.parent("sketch-container");
 
-  // Initialize p5.party
-
-  // Subscribe to 'newMessage' events
   partySubscribe("newMessage", displayMessage);
 
-  // Create the text input element
   input = createInput();
-  input.position(10, windowHeight - 50);
-  input.style("width", `${windowWidth - 20}px`);
+  input.parent("sketch-input");
+  input.style("width", "100%");
 
   // Attach an event listener for the 'Enter' key press
   input.elt.onkeypress = function (e) {
@@ -33,25 +31,18 @@ function setup() {
       sendMessage();
     }
   };
-}
 
-let messages = []; // Array to store received messages
-
-function displayMessage(message) {
-  messages.push(message); // Add the received message to the array
-  // You may want to limit the number of messages stored to avoid memory issues
-  if (messages.length > 100) {
-    // Just as an example limit
-    messages.shift(); // Remove the oldest message
-  }
+  window.addEventListener("resize", () => {
+    resizeCanvas(sketchContainer.offsetWidth, windowHeight - 36);
+  });
 }
 
 function draw() {
-  background(220);
+  background("#e1e1e1");
   drawGuestCursors();
-  // Display all messages
-  fill(0); // Set the text color to black
+  fill(0);
   noStroke();
+
   textAlign(LEFT, TOP);
   textSize(16);
 
@@ -74,6 +65,17 @@ function mouseMoved() {
   me.y = mouseY;
 }
 
+let messages = []; // Array to store received messages
+
+function displayMessage(message) {
+  messages.push(message); // Add the received message to the array
+  // You may want to limit the number of messages stored to avoid memory issues
+  if (messages.length > 100) {
+    // Just as an example limit
+    messages.shift(); // Remove the oldest message
+  }
+}
+
 function sendMessage() {
   let messageText = input.value().trim();
   if (messageText !== "") {
@@ -81,78 +83,3 @@ function sendMessage() {
     input.value(""); // Clear the input field
   }
 }
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  input.position(10, windowHeight - 50);
-  input.style("width", `${windowWidth - 20}px`);
-}
-
-// let input;
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   pixelDensity(2);
-//   textSize(17);
-
-//   // Initialize p5.party
-//   partyConnect("wss://demoserver.p5party.org", "p5_messaging_app", "main");
-
-//   // Subscribe to 'newMessage' events
-//   partySubscribe("newMessage", displayMessage);
-
-//   // Create the text input element
-//   input = createInput();
-//   input.position(10, windowHeight - 50);
-//   input.style("width", `${windowWidth - 20}px`);
-
-//   // Attach an event listener for the 'Enter' key press
-//   input.elt.onkeypress = function (e) {
-//     if (e.keyCode == 13) {
-//       e.preventDefault(); // Prevent the default action to avoid form submission
-//       sendMessage();
-//     }
-//   };
-// }
-
-// let messages = []; // Array to store received messages
-
-// function displayMessage(message) {
-//   messages.push(message); // Add the received message to the array
-//   // You may want to limit the number of messages stored to avoid memory issues
-//   if (messages.length > 100) {
-//     // Just as an example limit
-//     messages.shift(); // Remove the oldest message
-//   }
-// }
-
-// function draw() {
-//   background(220);
-//   ellipse(mouseX, mouseY, 20, 20);
-//   // Display all messages
-//   fill(0); // Set the text color to black
-//   noStroke();
-//   textAlign(LEFT, TOP);
-//   textSize(16);
-
-//   let y = 10;
-//   for (let i = messages.length - 1; i >= 0; i--) {
-//     // Iterate backwards to display the newest messages at the bottom
-//     text(messages[i], 10, y);
-//     y += 20; // Increment y position for the next message
-//   }
-// }
-
-// function sendMessage() {
-//   let messageText = input.value().trim();
-//   if (messageText !== "") {
-//     partyEmit("newMessage", messageText); // Emit the message event with the message text
-//     input.value(""); // Clear the input field
-//   }
-// }
-
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-//   input.position(10, windowHeight - 50);
-//   input.style("width", `${windowWidth - 20}px`);
-// }
